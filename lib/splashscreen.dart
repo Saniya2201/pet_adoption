16f257da74d8ef17db915748f001ab94e643c00e
Saniya <saniyabinoy22@gmail.com>
2022-11-09 23:40:54 +0530

    splashscreen

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -5,11 +5,11 @@ class SplashScreen extends StatelessWidget {
   const SplashScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return Center(
-      child: Container(
-        child: Lottie.asset('assets/splashscreen.json'),
-      ),
-    );
-  }
+  Widget build(BuildContext context) => Scaffold(
+    body: Center(
+        child: Container(
+          child: Lottie.asset('assets/splashscreen.json'),
+        ),
+    ),
+  );
 }
