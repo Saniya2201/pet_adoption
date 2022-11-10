ec907e749b0d587e5f09464526c9cc7dd1e8a0d9
Saniya <saniyabinoy22@gmail.com>
2022-11-10 18:53:41 +0530

    splashscreen

--- lib/splashscreen.dart
+++ lib/splashscreen.dart
@@ -1,14 +1,54 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
+import 'package:pet_adoption/signIn.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
   const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen>
+ with SingleTickerProviderStateMixin {
+  late AnimationController controller;
+
+  @override
+  void initState() {
+    super.initState();
+
+    controller = AnimationController(
+        vsync: this,
+    );
+
+    controller.addStatusListener((status) async {
+      if (status == AnimationStatus.completed) {
+        Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => SignIn()));
+      }
+    });
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) => Scaffold(
     body: Center(
         child: Container(
-          child: Lottie.asset('assets/splashscreen.json'),
+          child: Lottie.asset(
+              'assets/splashscreen.json',
+            repeat: false,
+            controller: controller,
+            onLoaded: (composition) {
+                controller.forward();
+            }
+          ),
         ),
     ),
   );
